3061838b071380826c31847dcf8ebccd122f999b
SemSreyleak11111111111111 <kanhaleak777@gmail.com>
2026-02-23 17:55:10 +0700

    change footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
 export default function Footer() {
   return (
-    <footer className="bg-black border-t border-gray-800 py-10 text-center text-gray-500">
-      <p>អរគុណបងដែលបានចំណាយពេលមើល</p>
-      {/* <p className="mt-2 text-sm">
-        © {new Date().getFullYear()} Sem Sreyleak — Graphic Designer & Visual Artist
-      </p> */}
+    <footer className="bg-black border-t border-gray-800 py-16 text-center">
+      <h2 className="text-4xl md:text-5xl font-bold bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 bg-clip-text text-transparent tracking-wide">
+        Thank You
+      </h2>
+
+      <p className="mt-4 text-gray-400 text-sm md:text-base">
+        Thank you for spending time to view my portfolio.
+      </p>
     </footer>
   );
 }
