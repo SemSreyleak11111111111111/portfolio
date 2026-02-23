977f6a0a5eb047b2d7f8c1cb7070f16fd61e31c2
SemSreyleak11111111111111 <kanhaleak777@gmail.com>
2026-02-23 19:26:49 +0700

    title

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,28 +4,23 @@ export const projectCategories = [
     title: "Food Posters",
     items: [
       {
-        title: "Burger Special",
-        category: "Food / Advertising",
+        title: "Khmer Fish",
         image: "https://i.pinimg.com/736x/f8/fe/0b/f8fe0bdc30eed5f0ec402d84960cff33.jpg",
       },
       {
-        title: "Cupcake Delight",
-        category: "Dessert Poster",
+        title: "Burger",
         image: "https://i.pinimg.com/736x/3c/09/92/3c099232e0cb4fc09a3f0cb4c457b1e9.jpg",
       },
       {
-        title: "Healthy Salad",
-        category: "Food Menu",
+        title: "Khmer Food Poster",
         image: "https://i.pinimg.com/736x/cc/5a/6c/cc5a6c68330fef708b8c4bbd7bf43e6d.jpg",
       },
       {
-        title: "Healthy Salad",
-        category: "Food Menu",
+        title: "Tomato",
         image: "https://i.pinimg.com/1200x/cd/46/71/cd4671aa10a2ac3af45d3a8e9803d97d.jpg",
       },
         {
-        title: "Healthy Salad",
-        category: "Food Menu",
+        title: "Momos",
         image: "https://i.pinimg.com/736x/d3/2a/36/d32a36ca44e21d2e1d0eaa1f083d3750.jpg",
       },
       // Add more of your own food posters here...
@@ -35,38 +30,31 @@ export const projectCategories = [
     title: "Drink Posters",
     items: [
       {
-        title: "Aperol Spritz",
-        category: "Cocktail Collection",
+        title: "Strawberry",
         image: "https://i.pinimg.com/736x/7f/af/96/7faf9649d0f48600c2db73d5f09d79c9.jpg",
       },
       {
-        title: "Espresso Martini",
-        category: "Beverage Art",
+        title: "Lemon Juice",
         image: "https://i.pinimg.com/736x/4b/1e/10/4b1e109520682e135b6aeefd11d9f770.jpg",
       },
       {
-        title: "Piña Colada",
-        category: "Tropical Drink",
+        title: "Energy Drink",
         image: "https://i.pinimg.com/736x/00/eb/e3/00ebe34b6bf335dd95c4877fc08ed29a.jpg",
       },
       {
-        title: "Piña Colada",
-        category: "Tropical Drink",
+        title: "Corner",
         image: "https://i.pinimg.com/736x/27/40/14/2740144ca7fd7ef974139db0edf5acc1.jpg",
       },
       {
-        title: "Piña Colada",
-        category: "Tropical Drink",
+        title: "Discount Drink",
         image: "https://i.pinimg.com/736x/13/5c/ca/135cca2c2bd8f37dc063dfff29fb6e4a.jpg",
       },
       {
-        title: "Piña Colada",
-        category: "Tropical Drink",
+        title: "Kolen Juice",
         image: "https://i.pinimg.com/1200x/8e/ed/78/8eed7842185fdc5b22628da263697a86.jpg",
       },
        {
-        title: "Piña Colada",
-        category: "Tropical Drink",
+        title: "Sprite",
         image: "https://i.pinimg.com/736x/d2/da/ec/d2daec2cd42684ef0ba1d7ed5841f759.jpg",
       },
       // Add your drink posters...
@@ -77,23 +65,19 @@ export const projectCategories = [
     items: [
       {
         title: " Running Shoes",
-        category: "Sneaker Collection",
         image: "https://i.pinimg.com/736x/d1/ca/47/d1ca471746b44f972d41ef8ff4b1083e.jpg",
       },
 
       {
         title: " Air Jordan",
-        category: "Streetwear Poster",
         image: "https://i.pinimg.com/1200x/f8/fd/fd/f8fdfd1dcba32e77df4078a284fc936d.jpg",
       },
       {
-        title: " Air Jordan",
-        category: "Streetwear Poster",
+        title: " Nike",
         image: "https://i.pinimg.com/736x/98/8e/3b/988e3bafb148ade368049c144a9a5169.jpg",
       },
       {
-        title: " Air Jordan",
-        category: "Streetwear Poster",
+        title: " White Shoe",
         image: "https://i.pinimg.com/736x/0f/53/3c/0f533c0b44441bc13bcfb1bef4c67e37.jpg",
       },
       // Add more shoe designs...
@@ -104,22 +88,18 @@ export const projectCategories = [
     items: [
       {
         title: "Sunscreen SPF 50",
-        category: "Automotive Series",
         image: "https://i.pinimg.com/736x/27/2b/b7/272bb728ded8745019a6f6d6b1977326.jpg",
       },
       {
         title: "Facefoam",
-        category: "Best cleancing",
         image: "https://i.pinimg.com/736x/0b/e6/7c/0be67c5c17816d4d991710ba0e9cbfdc.jpg",
       },
       {
         title: "Facefoam",
-        category: "Orange with horney",
         image: "https://i.pinimg.com/736x/e1/c6/66/e1c666b350ea13d94a36fc038526a144.jpg",
       },
       {
         title: "Facefoam",
-        category: "Orange with horney",
         image: "https://i.pinimg.com/736x/96/be/af/96beafdc70f36df3cae4d75d98dec425.jpg",
       },
       // Add your car posters...
@@ -130,23 +110,23 @@ export const projectCategories = [
     items: [
       {
         title: "Can Mock Up",
-        category: "Youtube tutorial",
         image: "https://i.pinimg.com/736x/c1/05/eb/c105ebc2fb7e2724bf3314a623e897f5.jpg",
       },
       {
-        title: "Facefoam",
-        category: "Best cleancing",
+        title: "Serum Bottle Mock up",
         image: "https://i.pinimg.com/1200x/32/96/65/329665e7259a96ac477abe46eecff0b0.jpg",
       },
       {
-        title: "Facefoam",
-        category: "Orange with horney",
+        title: "Yogurt Mock Up ",
         image: "https://i.pinimg.com/736x/0c/dd/d0/0cddd0d45b65e6a58ebef4f71aaeb854.jpg",
       },
       {
-        title: "Facefoam",
-        category: "Orange with horney",
+        title: "Brading And Packaging",
         image: "https://i.pinimg.com/736x/bb/10/09/bb10093924684e9051449d1317b132d6.jpg",
+      },
+        {
+        title: "Bag Mock Up",
+        image: "https://i.pinimg.com/1200x/b3/99/22/b3992270b843796a23e395ad645900d6.jpg",
       },
       // Add your car posters...
     ],
